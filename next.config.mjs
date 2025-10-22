9edc5e71d3a4e31cacac4646e1639899cdeff498
Bill Susanto <107653597+billsusanto@users.noreply.github.com>
2025-10-21 21:54:32 -0700

    fix: remove static export for dynamic routes support

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,7 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
     unoptimized: true
-  },
-  output: 'export',
-  trailingSlash: true
+  }
 };
 
 export default nextConfig;
